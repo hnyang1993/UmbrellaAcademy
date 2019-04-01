5d28942d5024fd22c729b40196751a99773fccf8
yutong1307 <yutong1307@gmail.com>
2019-03-31 23:58:07 -0400

    update

--- preprocess.R
+++ preprocess.R
@@ -3,7 +3,9 @@ library(data.table)
 library(magrittr)
 library(stopwords)
 library(SnowballC)
-
+library(caret)
+library(glmnet)
+setwd("~/Desktop/BIOS 735/final_project/jigsaw-toxic-comment-classification-challenge")
 sample       <- fread("sample_submission.csv")
 test         <- fread("test.csv")
 test_labels  <- fread("test_labels.csv")
@@ -27,7 +29,20 @@ vocab = create_vocabulary(it_train, ngram=c(1L,3L), stopwords=stopwords("en",sou
 prune.vocab <- prune_vocabulary(vocab, term_count_min = 10)
 
 vectorizer = vocab_vectorizer(prune.vocab)
-dtm_train = create_dtm(it_train, vectorizer)
-dim(dtm_train)
-colnames(dtm_train)[1:10]
-dtm_train[1:10,1:10]
+raw.dtm = create_dtm(it_train, vectorizer)
+dim(raw.dtm)
+
+bns = BNS$new()
+train.dtm = bns$fit_transform(raw.dtm, train.data$toxic)
+train.dtm = raw.dtm
+
+feature.selection = cv.glmnet(train.dtm, train.data$toxic, family = "binomial")
+
+fit = glmnet(train.dtm, train.data$toxic, family = "binomial",
+             alpha=1,lambda=feature.selection$lambda.1se)
+
+beta = fit$beta
+
+s.train <- train.dtm[,which(beta!=0)]
+
+dense.train.dtm <- as.matrix(s.train)
