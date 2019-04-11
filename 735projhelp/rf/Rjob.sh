7a79792a40d8b4d358b8647feb18992d85ca902f
hnyang1993 <hnyang@live.unc.edu>
2019-04-11 17:31:45 -0400

    help

---
+++ 735projhelp/rf/Rjob.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+NUMJOBS="$1"
+
+for i in `seq $NUMJOBS`;
+do
+
+#PROGRAM="count.sh"
+#PROGRAM="mycode.exe `cat mydata-$i.csv`"
+mkdir $i 
+cp *.R ./$i
+cd ./$i
+
+sbatch -p general -N 1 --mem=5g -n 1 -t 24:00:00 --job-name=“rfcv” R CMD BATCH --no-save Mar_Freq_rf.R Mar_Freq_rf.Rout
+
+cd ../
+done
