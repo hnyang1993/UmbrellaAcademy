49d0f30acf1e0ee983515f2d8b983dea53c576f2
hnyang1993 <hnyang@live.unc.edu>
2019-04-12 11:54:26 -0400

    update

---
+++ Mar_Freq_svm.R
@@ -0,0 +1,28 @@
+## generate seed by directory name
+path <- getwd()
+seed <- as.numeric(tail(unlist(strsplit(path, '/')), 1))
+set.seed(seed)
+filename <- paste("svmCV",seed,".RData",sep="")
+
+
+library(Matrix)
+library(data.table)
+train_mar <- readMM("/nas/longleaf/home/hnyang/735proj/svm/train_mar.txt")
+train_mar <- as.matrix(train_mar)
+keyword <- read.csv("/nas/longleaf/home/hnyang/735proj/svm/key_word_list.csv", stringsAsFactors = FALSE)
+colnames(train_mar) <- keyword[,2]
+train_mar_id <- read.csv("/nas/longleaf/home/hnyang/735proj/svm/train_mar_id.csv", stringsAsFactors=FALSE)
+train_mar_y <- read.csv("/nas/longleaf/home/hnyang/735proj/svm/train_mar_label.csv", stringsAsFactors=FALSE)
+
+train_mar_y <- train_mar_y$toxic
+
+library(caret)
+svmGrid <- expand.grid(C=seq(10,100,by=10))
+
+trCtl <- trainControl(method="cv", number=10, savePredictions=FALSE)
+fit <- train(train_mar, as.factor(train_mar_y), method="svmLinear", trControl=trCtl, tuneGrid = svmGrid)
+
+result <- fit$result
+
+## save output to .RData file
+save(result, file = filename)
