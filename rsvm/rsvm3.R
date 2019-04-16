a7837d69cfceee968589256015875f4d4e0f9157
yutong1307 <yutong1307@gmail.com>
2019-04-15 20:45:25 -0400

    radial svm 3rd result

---
+++ rsvm/rsvm3.R
@@ -0,0 +1,28 @@
+library(text2vec)
+library(data.table)
+library(magrittr)
+library(stopwords)
+library(SnowballC)
+library(caret)
+library(glmnet)
+library(kernlab)
+
+setwd("/nas/longleaf/home/yutongl/BIOS735/svm")
+
+load("prelasso.RData")
+
+## svmRadial--based on terms selected from lasso
+set.seed(111)
+trCtl = trainControl(method = "cv") 
+tg <- expand.grid(C = seq(10,100,10), sigma = c(5,7,9))
+fit_svmRadial = train(x = dense.train.dtm, 
+                      y = as.factor(train.sample$toxic), 
+                      method = "svmRadial", 
+                      tuneGrid = tg,
+                      trControl = trCtl)
+pred_svmRadial = predict(fit_svmRadial, as.matrix(test.dtm))
+confusionMatrix(pred_svmRadial, as.factor(test.labels$toxic), positive = "1")
+
+
+
+save(fit_svmRadial, file = "./rsvm3.RData")
