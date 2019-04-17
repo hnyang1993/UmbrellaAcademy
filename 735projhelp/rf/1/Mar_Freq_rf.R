5e24de2b4074b264dfd029b6946cc2b389f3ca56
yutong1307 <yutong1307@gmail.com>
2019-04-17 13:38:54 -0400

    rf result

---
+++ 735projhelp/rf/1/Mar_Freq_rf.R
@@ -0,0 +1,28 @@
+## generate seed by directory name
+path <- getwd()
+seed <- as.numeric(tail(unlist(strsplit(path, '/')), 1))
+set.seed(seed)
+filename <- paste("rfCV",seed,".RData",sep="")
+
+
+library(Matrix)
+library(data.table)
+train_mar <- readMM("/nas/longleaf/home/yutongl/BIOS735/735projhelp/rf/train_mar.txt")
+train_mar <- as.matrix(train_mar)
+keyword <- read.csv("/nas/longleaf/home/yutongl/BIOS735/735projhelp/rf/key_word_list.csv", stringsAsFactors = FALSE)
+colnames(train_mar) <- keyword[,2]
+train_mar_id <- read.csv("/nas/longleaf/home/yutongl/BIOS735/735projhelp/rf/train_mar_id.csv", stringsAsFactors=FALSE)
+train_mar_y <- read.csv("/nas/longleaf/home/yutongl/BIOS735/735projhelp/rf/train_mar_label.csv", stringsAsFactors=FALSE)
+
+train_mar_y <- train_mar_y$toxic
+
+library(caret)
+rfGrid <- expand.grid(mtry=c(2, 4, 8, 12, 16, 20, 24, 28, 32, 36))
+
+trCtl <- trainControl(method="cv", number=10, savePredictions=FALSE)
+fit <- train(train_mar, as.factor(train_mar_y), method="rf", trControl=trCtl, tuneGrid = rfGrid)
+
+result <- fit
+
+## save output to .RData file
+save(result, file = filename)
