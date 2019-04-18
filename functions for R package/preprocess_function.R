26c807e282bac263aa2c19c73bd43baa8c665dd8
yutong1307 <yutong1307@gmail.com>
2019-04-18 13:38:29 -0400

    function update pre

--- functions for R package/preprocess_function.R
+++ functions for R package/preprocess_function.R
@@ -1,23 +1,31 @@
-prep_fun = tolower
 #stemming words
 tok_fun = function(x) {
   word_tokenizer(x) %>% lapply( function(x) SnowballC::wordStem(x,language="en"))
 }
 
-preprocess <- function(data = train.sample, prep_fun = tolower, tok_fun = tok_fun){
-  it_data = itoken(data$comment_text,
+preprocess <- function(traindata, testdata, prep_fun = tolower, tok_fun = tok_fun){
+  it_train = itoken(traindata$comment_text,
                     preprocessor = prep_fun,
                     tokenizer = tok_fun,
-                    ids = data$id,
+                    ids = traindata$id,
                     progressbar = TRUE)
   
   
-  vocab = create_vocabulary(it_data, ngram=c(1L,3L), stopwords=stopwords("en",source="smart"))
+  vocab = create_vocabulary(it_train, ngram=c(1L,3L), stopwords=stopwords("en",source="smart"))
   
   prune.vocab <- prune_vocabulary(vocab, term_count_min = 10)
   
   vectorizer = vocab_vectorizer(prune.vocab)
-  raw.dtm = create_dtm(it_data, vectorizer)
+  raw.dtm = create_dtm(it_train, vectorizer)
   
-  return(raw.dtm)
+  it_test = itoken(testdata$comment_text,
+                   preprocessor = prep_fun,
+                   tokenizer = tok_fun,
+                   ids = testdata$id,
+                   progressbar = TRUE)
+  
+  raw.test.dtm = create_dtm(it_test, vectorizer)
+
+  return(list("train.dtm" = "raw.dtm",
+              "test.dtm"  = "raw.test.dtm")) 
 }
