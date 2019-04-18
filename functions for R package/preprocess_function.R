9ae78f6417768e8cbc757696df372cb1abd69e4e
yutong1307 <yutong1307@gmail.com>
2019-04-18 13:31:19 -0400

    function

--- functions for R package/preprocess_function.R
+++ functions for R package/preprocess_function.R
@@ -5,19 +5,19 @@ tok_fun = function(x) {
 }
 
 preprocess <- function(data = train.sample, prep_fun = tolower, tok_fun = tok_fun){
-  it_train = itoken(data$comment_text,
+  it_data = itoken(data$comment_text,
                     preprocessor = prep_fun,
                     tokenizer = tok_fun,
                     ids = data$id,
                     progressbar = TRUE)
   
   
-  vocab = create_vocabulary(it_train, ngram=c(1L,3L), stopwords=stopwords("en",source="smart"))
+  vocab = create_vocabulary(it_data, ngram=c(1L,3L), stopwords=stopwords("en",source="smart"))
   
   prune.vocab <- prune_vocabulary(vocab, term_count_min = 10)
   
   vectorizer = vocab_vectorizer(prune.vocab)
-  raw.dtm = create_dtm(it_train, vectorizer)
+  raw.dtm = create_dtm(it_data, vectorizer)
   
   return(raw.dtm)
 }
