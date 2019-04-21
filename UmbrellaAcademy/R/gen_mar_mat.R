93e32a026e353a4b1c3949520156a30fd207f28e
mochuan4233 <mochuan.liu@gmail.com>
2019-04-20 22:51:35 -0400

    Fixed bugs and added documentations for functions Marginal_Freq and comment_matrix

--- UmbrellaAcademy/R/gen_mar_mat.R
+++ UmbrellaAcademy/R/gen_mar_mat.R
@@ -22,8 +22,13 @@
 #' @import svMisc
 #'
 #' @export
-comment_matrix <- function(commentData, key.word.list, colname = 'comment_text', progress = TRUE){
-
+comment_matrix <- function(commentData, key.word.list, id = 'id', colname = 'comment_text', progress = TRUE){
+  
+  ## Testing functions
+  if(!is.data.frame(commentData))  stop("Input data is not a dataframe")
+  if(is.null(commentData[, id])) stop("Id is missing")
+  if(is.null(commentData[, colname])) stop("Comment text is missing")
+  
   ## Package
   #require('Matrix')
   #require('stringr')
@@ -33,9 +38,17 @@ comment_matrix <- function(commentData, key.word.list, colname = 'comment_text',
   ## Change column name to default 'id'
   col.id <- which(names(commentData)== id)
   names(commentData)[col.id] <- 'id'
+  
+  ## Change column name to default 'coment_text'
+  col.comment <- which(names(commentData)== colname)
+  names(commentData)[col.comment] <- 'comment_text'
 
   K <- length(key.word.list)
   N <- dim(commentData)[1]
+  
+  check_point <- 0
+  
+  if(dim(commentData)[1]<=1000) progress <- FALSE
 
   if(progress == TRUE){
     pb <- txtProgressBar(min = 0, max = 1, initial = 0, style = 3)
@@ -65,7 +78,7 @@ comment_matrix <- function(commentData, key.word.list, colname = 'comment_text',
     }
   }
 
-  if(progess == TRUE)   close(pb)
+  if(progress == TRUE) close(pb)
 
   rownames(count.matrix) <- col.id
   colnames(count.matrix) <- key.word.list
