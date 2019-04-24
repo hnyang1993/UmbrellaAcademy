4b00292c45e17baa064d5862f33412dab53e0dbe
yutong1307 <yutong1307@gmail.com>
2019-04-24 00:21:25 -0400

    update results

---
+++ 735projhelp/svmnew2/ss7.R
@@ -0,0 +1,24 @@
+setwd("/nas/longleaf/home/yutongl/BIOS735/735projhelp/svmnew2")
+set.seed(111)
+library(Matrix)
+library(data.table)
+train_mar <- readMM("/nas/longleaf/home/yutongl/BIOS735/735projhelp/svm/train_mar.txt")
+train_mar <- as.matrix(train_mar)
+keyword <- read.csv("/nas/longleaf/home/yutongl/BIOS735/735projhelp/svm/key_word_list.csv", stringsAsFactors = FALSE)
+colnames(train_mar) <- keyword[,2]
+train_mar_id <- read.csv("/nas/longleaf/home/yutongl/BIOS735/735projhelp/svm/train_mar_id.csv", stringsAsFactors=FALSE)
+train_mar_y <- read.csv("/nas/longleaf/home/yutongl/BIOS735/735projhelp/svm/train_mar_label.csv", stringsAsFactors=FALSE)
+
+train_mar_y <- train_mar_y$toxic
+
+train_mar_full <- cbind(train_mar_y, train_mar)
+train_mar_full <- as.data.frame(train_mar_full)
+train_mar_full <-  train_mar_full[,-which(sapply(train_mar_full, function(v) var(v, na.rm=TRUE)==0))]
+library(e1071)
+
+fit <- tune.svm(as.factor(train_mar_y)~., data = train_mar_full, gamma = 2^(-8), cost = 20)
+
+result7 <- fit
+
+## save output to .RData file
+save(result7, file = "/nas/longleaf/home/yutongl/BIOS735/735projhelp/svmnew2/s7.RData")
